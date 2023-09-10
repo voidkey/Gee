ed6869801721393457a089d7be1d3100e83e2cf6
nulkey <xzfnku@outlook.com>
2023-09-10 23:50:13 +0800

    Middlewares

---
+++ 5-middleware/gee/context.go
@@ -0,0 +1,98 @@
+package gee
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type H map[string]interface{}
+
+type Context struct {
+	//origin objects
+	Writer http.ResponseWriter
+	Req    *http.Request
+
+	//request info
+	Path   string
+	Method string
+	Params map[string]string
+
+	//response info
+	StatusCode int
+
+	//middleware
+	handlers []HandlerFunc
+	index    int
+}
+
+func newContext(w http.ResponseWriter, r *http.Request) *Context {
+	return &Context{
+		Writer: w,
+		Req:    r,
+		Path:   r.URL.Path,
+		Method: r.Method,
+		//StatusCode: r.Response.StatusCode,
+		index: -1,
+	}
+}
+func (ctx *Context) Next() {
+	ctx.index++
+	s := len(ctx.handlers)
+	for ; ctx.index < s; ctx.index++ {
+		ctx.handlers[ctx.index](ctx)
+	}
+}
+
+func (ctx *Context) Fail(code int, err string) {
+	ctx.index = len(ctx.handlers)
+	ctx.JSON(code, H{"message": err})
+}
+
+func (ctx *Context) PostForm(key string) string {
+	return ctx.Req.FormValue(key)
+}
+
+func (ctx *Context) Query(key string) string {
+	return ctx.Req.URL.Query().Get(key)
+}
+
+func (ctx *Context) Status(code int) {
+	ctx.StatusCode = code
+	ctx.Writer.WriteHeader(code)
+}
+
+func (ctx *Context) SetHeader(key string, value string) {
+	ctx.Writer.Header().Set(key, value)
+}
+
+func (ctx *Context) String(code int, format string, values ...interface{}) {
+	ctx.SetHeader("Content-Type", "text/plain")
+	ctx.Status(code)
+	ctx.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+}
+
+func (ctx *Context) JSON(code int, obj interface{}) {
+	ctx.SetHeader("Content-Type", "application/json")
+	ctx.Status(code)
+	encoder := json.NewEncoder(ctx.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(ctx.Writer, err.Error(), 500)
+	}
+}
+
+func (ctx *Context) Data(code int, data []byte) {
+	ctx.Status(code)
+	ctx.Writer.Write(data)
+}
+
+func (ctx *Context) HTML(code int, html string) {
+	ctx.SetHeader("Content-Type", "text/html")
+	ctx.Status(code)
+	ctx.Writer.Write([]byte(html))
+}
+
+func (ctx *Context) Param(key string) string {
+	value := ctx.Params[key]
+	return value
+}
---
+++ 5-middleware/gee/gee.go
@@ -0,0 +1,78 @@
+package gee
+
+import (
+	"log"
+	"net/http"
+	"strings"
+)
+
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(ctx *Context)
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc //support middleware
+	parent      *RouterGroup  //support nesting
+	engine      *Engine       //all groups share a Engine instance
+}
+
+// Engine implements the interface of ServeHTTP,replace of DefaultServeMux
+type Engine struct {
+	router *router
+	*RouterGroup
+	groups []*RouterGroup // store all groups
+}
+
+// New is the constructor of gee.Engine
+func New() *Engine {
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+// Use is defined to add middleware to the group
+func (group *RouterGroup) Use(middlewares ...HandlerFunc) {
+	group.middlewares = append(group.middlewares, middlewares...)
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var middlewares []HandlerFunc
+	for _, group := range engine.groups {
+		if strings.HasPrefix(req.URL.Path, group.prefix) {
+			middlewares = append(middlewares, group.middlewares...)
+		}
+	}
+	c := newContext(w, req)
+	c.handlers = middlewares
+	engine.router.handle(c)
+}
